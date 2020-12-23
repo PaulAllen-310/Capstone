3c3daad6bf1256e584d185cece6e9e3d1c708c41
PaulAllen-310 <paul.allen310@googlemail.com>
2020-12-23 14:45:44 +1100

    Added Zokrates Verifier

--- eth-contracts/config/testConfig.js
+++ eth-contracts/config/testConfig.js
@@ -1,12 +1,15 @@
 var ERC721Mintable = artifacts.require("./ERC721Mintable.sol");
+var Verifier = artifacts.require("./verifier.sol");
 
 var Config = async function (accounts) {
     const owner = accounts[0];
     const erc721Mintable = await ERC721Mintable.new();
+    const verifier = await Verifier.new();
 
     return {
-        owner: owner,
         erc721Mintable: erc721Mintable,
+        verifier: verifier,
+        owner: owner,
         noOfTokensToMint: 10,
         transferTo: accounts[1],
     };
